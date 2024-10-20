44dd0b73c9044e655acf580572cb87497c75ed26
BharatNaik24 <159719274+BharatNaik24@users.noreply.github.com>
2024-10-20 07:57:59 +0530

    page error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import BrowserRouter
-import "./App.css";
 import Header from "./components/header/header";
 import LandingPage from "./components/landingPage/landingPage";
 import Filter from "./components/filter/filter";
 import Products from "./components/products/products";
 import Footer from "./components/footer/footer";
 import { ToastContainer } from "react-toastify";
-
 import { ProductProvider } from "./context/ProductContext";
 import { plantData } from "./plantData/plantData";
+
+import "./App.css";
 import ThankYouPage from "./pages/thankYouPage/thankyoupage";
 
 function App() {
--- src/pages/thankyoupage/thankyoupage.jsx
+++ src/pages/thankyoupage/thankyoupage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useProduct } from "../../context/ProductContext";
 import "./thankyoupage.css";
 import { MdAttachEmail } from "react-icons/md";
